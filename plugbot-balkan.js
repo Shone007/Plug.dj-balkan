ea453e4a6bca6f3fdbd46e9e77927d45e9bd8158
Shone007 <bojanicneso@gmail.com>
2013-12-27 20:49:27 +0100

    Update plugbot-balkan.js

--- plugbot-balkan.js
+++ plugbot-balkan.js
@@ -1,4 +1,4 @@
-<span class="text">Dobro dosli na Plug.dj.Verzija 0.9.817</span>
+<span class="text">Dobro dosli na Plug.dj.Verzija 0.9.817
 var baseUrl = "http://i.imgur.com/";
 var wooting = true;
  
