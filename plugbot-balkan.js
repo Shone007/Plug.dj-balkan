280116da2edbc2ff2c33fc4cc7110f7219efbe76
Shone007 <bojanicneso@gmail.com>
2013-12-30 21:50:12 +0100

    Update plugbot-balkan.js

--- plugbot-balkan.js
+++ plugbot-balkan.js
@@ -35,7 +35,7 @@ function stopWooting() {
         $('#autowooter-js').remove();
         $('#autowooter-css').remove();
         $('#autowooter-button').remove();
-API.sendChat('/me koristi autoChat v1.0 !');
+API.sendChat('/me koristi autoChat v1.1 !');
 }
  
 function DJ_ADVANCE_LISTENER(obj) {
