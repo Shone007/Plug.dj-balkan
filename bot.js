f61c70aec7e8f3d36482623178669bc553dc2aba
Shone007 <bojanicneso@gmail.com>
2014-04-20 21:58:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,5 @@
 if (API.enabled && $("#test-css").length <= 0) {
 $('body').prepend('<link rel="stylesheet" type="text/css" id="test-css" href="https://raw.githubusercontent.com/Shone007/PlugBot/master/bot.css" />');
+$('#room').append(content);     
+    $('#room').append(content2); 
+    $('#room').append(content3); 
